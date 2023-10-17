72d348bc83009a66281a1de85ef8c762036cca5d
OchandoDevSergio <sergio.ochando.dev@gmail.com>
2023-10-17 12:26:22 +0200

    Functionality for new appointment improved with roleId distinction

--- src/pages/AppointmentAdd/AppointmentAdd.jsx
+++ src/pages/AppointmentAdd/AppointmentAdd.jsx
@@ -1,232 +1,224 @@
-import './AppointmentAdd.css'
+import "./AppointmentAdd.css";
 import { userDataCheck } from "../../pages/userSlice";
 import { useSelector } from "react-redux";
 import { Input } from "../../common/Input/Input";
 import { useNavigate } from "react-router-dom";
 import { createAppointment } from "../../services/apiCalls";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { paymentDataCheck, loadPaymentData } from "../paymentSlice";
-import { searchCustomerPayment } from '../../services/apiCalls';
+import { searchCustomerPayment } from "../../services/apiCalls";
 import { useDispatch } from "react-redux";
-import { bringArtists } from '../../services/apiCalls';
+import { bringArtists } from "../../services/apiCalls";
 
 export const AppointmentAdd = () => {
-    //Instanciamos Redux en modo LECTURA user
-    const reduxUserData = useSelector(userDataCheck);
+  //Instanciamos Redux en modo LECTURA user
+  const reduxUserData = useSelector(userDataCheck);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    //Código para traer los datos de pago
-    const customerId =reduxUserData?.credentials?.userData?.userId;
+  //Código para traer los datos de pago
+  const customerId = reduxUserData?.credentials?.userData?.userId;
 
-    const searchPaymentData = (paymentDatas) => {
-      dispatch(loadPaymentData({ paymentDataData: paymentDatas }));
-    };
+  const searchPaymentData = (paymentDatas) => {
+    dispatch(loadPaymentData({ paymentDataData: paymentDatas }));
+  };
 
-    const tokenPayment = reduxUserData.credentials.token;
+  const tokenPayment = reduxUserData.credentials.token;
 
+  useEffect(() => {
+    searchCustomerPayment(customerId, tokenPayment).then((results) => {
+      searchPaymentData(results);
+    });
+  }, [customerId]);
 
-    useEffect(() => {
-      
-      searchCustomerPayment(customerId, tokenPayment)      
-      .then((results) => {
-        searchPaymentData(results);
+  const reduxPaymentData = useSelector(paymentDataCheck);
+
+  const navigate = useNavigate();
+  const [newAppointmentBody, setNewAppointmentBody] = useState({
+    user_id: reduxUserData.credentials.userData.roleId === 1 ? "" : customerId,
+    artist_id: "",
+    date: "",
+    hour: "",
+  });
+
+  const [cardSecurity, setCardSecurity] = useState({
+    safeNumber: "",
+  });
+
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    bringArtists()
+      .then((resultado) => {
+        setArtists(resultado.data.data);
       })
-    }, [customerId]);
+      .catch((error) => console.log(error));
+  }, []);
 
-    const reduxPaymentData = useSelector(paymentDataCheck);
+  const [selectedArtist, setSelectedArtist] = useState([]);
 
-    const navigate = useNavigate();
-    const [newAppointmentBody, setNewAppointmentBody] = useState({
-        user_id: "",
-        artist_id: "",
-        date: "",
-        hour: "",
-        });
-    
-    const [cardSecurity, setCardSecurity] = useState({
-        safeNumber:""
-          });
-
-    const [artists, setArtists] = useState([]);
-
-    useEffect(() => {
-      bringArtists()
-      .then(
-        resultado => {
-          setArtists(resultado.data.data)
-        }
-      )
-      .catch(error => console.log(error))
-    }, []);
-
-    console.log(artists)
-
-    const [selectedArtist, setSelectedArtist] = useState([]);
-
-    useEffect(() => {
-      console.log(selectedArtist);
-    }, [selectedArtist]);
-
-    //BINDEO
-    const inputHandler = (e) => {
-        setNewAppointmentBody((prevState) => ({
-        ...prevState,
-        [e.target.name]: e.target.value,
-      }));
-    };
-
-    const inputHandlerSecurity = (e) => {
-      setCardSecurity((prevState) => ({
+  //BINDEO
+  const inputHandler = (e) => {
+    setNewAppointmentBody((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
   };
 
-    const registerAppointment = () => {
-       console.log("entro", newAppointmentBody)
-        createAppointment(newAppointmentBody, reduxUserData.credentials)
-        navigate("/appointments");
-        };
-      
-    return (
+  const inputHandlerSecurity = (e) => {
+    setCardSecurity((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const registerAppointment = () => {
+    console.log("entro", newAppointmentBody);
+    createAppointment(newAppointmentBody, reduxUserData.credentials);
+    navigate("/appointments");
+  };
+
+  return (
+    <>
+      {reduxUserData.credentials.userData.roleId === 1 && (
         <>
-        {reduxUserData.credentials.userData.roleId === 1 && (
-            <>
-            <div className="container-fluid register">
-              <div className="row upRowRegister">
-                <div className="col-1"></div>
-                <div className="col-5">
-                  <div className="row inputRow">
-                    <Input
+          <div className="container-fluid register">
+            <div className="row upRowRegister">
+              <div className="col-1"></div>
+              <div className="col-5">
+                <div className="row inputRow">
+                  <Input
                     type={"text"}
                     placeholder="Introduce the customer user identification number"
                     value={newAppointmentBody.user_id}
                     name={"user_id"}
                     className="defaultInput"
                     manejadora={inputHandler}
-                    />
-                  </div>
-                  <div className="row inputRow">
-                    <Input
+                  />
+                </div>
+                <div className="row inputRow">
+                  <Input
                     type={"text"}
                     placeholder="Introduce the artist identification number"
                     value={newAppointmentBody.artist_id}
                     name={"artist_id"}
                     className="defaultInput"
                     manejadora={inputHandler}
-                    />
-                  </div>
-                  <div className="row inputRow">
-                    <Input
+                  />
+                </div>
+                <div className="row inputRow">
+                  <Input
                     type={"text"}
                     placeholder="Introduce the appointment date"
                     value={newAppointmentBody.date}
                     name={"date"}
                     className="defaultInput"
                     manejadora={inputHandler}
-                    />
-                  </div>
-                  <div className="row inputRow">
-                    <Input
+                  />
+                </div>
+                <div className="row inputRow">
+                  <Input
                     type={"text"}
                     placeholder="Introduce the appointment hour"
                     value={newAppointmentBody.hour}
                     name={"hour"}
                     className="defaultInput"
                     manejadora={inputHandler}
-                    />
-                  </div>
+                  />
                 </div>
-                <div className="col-1"></div>
               </div>
-              <div className="row downRowRegister">
-                <div className="buttonBody" onClick={() => registerAppointment()}>
+              <div className="col-1"></div>
+            </div>
+            <div className="row downRowRegister">
+              <div className="buttonBody" onClick={() => registerAppointment()}>
                 Register appointment
-                </div>
               </div>
             </div>
-            </>
-        )}
-        {reduxUserData.credentials.userData.roleId === 2 && (
-            <>
-            <div className="container-fluid registerAppointment">
-              <div className='row subheaderRow'></div>
-              <div className="row upRowAppointment">
-                <div className='col'>
-                  <div className="row inputRow artistDropdown">
-                    {/* <select value={selectedArtist} onChange={e=>setSelectedArtist(e.target.value)}>
+          </div>
+        </>
+      )}
+      {reduxUserData.credentials.userData.roleId === 2 && (
+        <>
+          <div className="container-fluid registerAppointment">
+            <div className="row subheaderRow"></div>
+            <div className="row upRowAppointment">
+              <div className="col">
+                <div className="row inputRow artistDropdown">
+                  {/* <select value={selectedArtist} onChange={e=>setSelectedArtist(e.target.value)}>
                        {
                         artists.map(opt=><option>{opt}</option>)
                        }
                     </select> */}
-                  </div>
                 </div>
-                <div className='col'>
-                  <div className="row inputRow">
-                    <Input
+              </div>
+              <div className="col">
+                <div className="row inputRow">
+                  <Input
                     type={"text"}
                     placeholder="Introduce the appointment date"
                     value={newAppointmentBody.date}
                     name={"date"}
                     className="defaultInput"
                     manejadora={inputHandler}
-                    />
-                  </div>
-                  <div className="row inputRow">
-                    <Input
+                  />
+                </div>
+                <div className="row inputRow">
+                  <Input
                     type={"text"}
                     placeholder="Introduce the appointment hour"
                     value={newAppointmentBody.hour}
                     name={"hour"}
                     className="defaultInput"
                     manejadora={inputHandler}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="row midRowPayment">
-                <div className='row price'>
-                  <div className="row inputRow title">
-                  To make an appointment, a €20 deposit is required
-                  </div>
-                </div>
-                <div className='row paymentDataShow'>
-                  <div className='col-2'></div>
-                  <div className='col'>
-                  <div className="row inputRow title">
-                  Card Number: {reduxPaymentData.paymentDataData?.data?.data?.cardNumber}
-                  </div>
-                  <div className="row inputRow title">
-                  Vaid thru: {reduxPaymentData.paymentDataData?.data?.data?.validThru}
-                  </div>
-                  </div>
-                  <div className='col'>
-                  <div className="row inputRow">
-                    <Input
-                    type={"number"}
-                    placeholder="Introduce the CVV/CVC number"
-                    value= {cardSecurity.safeNumber}
-                    name={"safeNumber"}
-                    className="defaultInput"
-                    manejadora={inputHandlerSecurity}
-                    />
-                  </div>
-                  <div className="row inputRow title">
-                  Please check that your payment details are correct, otherwise you can modify them in your user profile
-                  </div>
-                  </div>
-                  <div className='col-2'></div>
-                </div>
-              </div>  
-              <div className="row downRowRegister">
-                <div className="buttonBody" onClick={() => registerAppointment()}>
-                Register appointment
+                  />
                 </div>
               </div>
             </div>
-            </>
-        )}
+            <div className="row midRowPayment">
+              <div className="row price">
+                <div className="row inputRow title">
+                  To make an appointment, a €20 deposit is required
+                </div>
+              </div>
+              <div className="row paymentDataShow">
+                <div className="col-2"></div>
+                <div className="col">
+                  <div className="row inputRow title">
+                    Card Number:{" "}
+                    {reduxPaymentData.paymentDataData?.data?.data?.cardNumber}
+                  </div>
+                  <div className="row inputRow title">
+                    Vaid thru:{" "}
+                    {reduxPaymentData.paymentDataData?.data?.data?.validThru}
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="row inputRow">
+                    <Input
+                      type={"number"}
+                      placeholder="Introduce the CVV/CVC number"
+                      value={cardSecurity.safeNumber}
+                      name={"safeNumber"}
+                      className="defaultInput"
+                      manejadora={inputHandlerSecurity}
+                    />
+                  </div>
+                  <div className="row inputRow title">
+                    Please check that your payment details are correct,
+                    otherwise you can modify them in your user profile
+                  </div>
+                </div>
+                <div className="col-2"></div>
+              </div>
+            </div>
+            <div className="row downRowRegister">
+              <div className="buttonBody" onClick={() => registerAppointment()}>
+                Register appointment
+              </div>
+            </div>
+          </div>
         </>
-    )
-}
+      )}
+    </>
+  );
+};
